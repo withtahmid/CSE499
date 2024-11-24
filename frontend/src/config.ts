0d2d6a1749448fde46f9ce17c0bd53e446b64e96
Sarwar Alam <sarowar.minhaj@northsouth.edu>
2024-11-25 02:49:25 +0600

    upd backend uri

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
 export const baseFrontendURI = "http://localhost:5173/";
-export const baseBackendURI = "http://localhost:3000";
+export const baseBackendURI = "https://cse-499-sarwar-sarwar76200s-projects.vercel.app"; //http://localhost:3000
